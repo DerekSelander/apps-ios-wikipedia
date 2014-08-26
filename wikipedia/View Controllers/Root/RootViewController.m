305eff93f9c067277c28502bcddd0f13dcfccdf9
Derek Selander <derekselander@yahoo.com>
2014-08-26 01:36:44 -0600

    Cleanup

--- wikipedia/View Controllers/Root/RootViewController.m
+++ wikipedia/View Controllers/Root/RootViewController.m
@@ -385,8 +385,6 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [[UIApplication sharedApplication] setStatusBarHidden:isStatusBarHidden withAnimation:UIStatusBarAnimationSlide];
     });
-
-    [self setNeedsStatusBarAppearanceUpdate];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
